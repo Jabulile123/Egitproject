acf77c0fc66320042c44136edd673195011814d6
Mbusi <sitolejp7@gmail.com>
2016-04-14 20:51:17 +0200

    My Second Commit after making changes in my code

--- Students/src/Module16/Git2.java
+++ Students/src/Module16/Git2.java
@@ -5,6 +5,7 @@ public class Git2 {
 	
 		public static void displaygreetings(){
 		System.out.println("Welcome to Our Database");
+		System.out.println("Hope You Are Happy With Our System");
 		}
 		}
 
