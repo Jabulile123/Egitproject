34e5bf3f0bae66701748800f5defb98cb0584982
Mbusi <sitolejp7@gmail.com>
2016-04-14 20:24:30 +0200

    My First Commit

---
+++ Students/src/Module16/Egitproject.java
@@ -0,0 +1,13 @@
+package Module16;
+
+public class Egitproject {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Students/src/Module16/Git2.java
@@ -0,0 +1,10 @@
+package Module16;
+
+public class Git2 {
+
+	
+		public static void displaygreetings(){
+		System.out.println("Welcome to Our Database");
+		}
+		}
+
